bee02537583d69abbc8f96932e384bbe6295d7f7
majdshatti <majdshatti8@gmail.com>
2022-07-17 18:10:16 +0300

    response messages datatype is added

---
+++ src/types/messages.types.ts
@@ -0,0 +1,14 @@
+export type messageTypes = {
+  special: string;
+  required: string;
+  email: string;
+  number: string;
+  zip: string;
+  unique: string;
+  minLength: string;
+  maxLength: string;
+  boolean: string;
+  date: string;
+  string: string;
+  betweenLength: string;
+};
