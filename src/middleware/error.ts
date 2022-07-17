26026ee04ea1a534cd209b3f54bca24505583500
majdshatti <majdshatti8@gmail.com>
2022-07-17 18:03:58 +0300

    Error handling middleware added

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import ErrorResponse from "../utils/errorResponse";
+import ErrorResponse from "../utils/error/errorResponse";
 import IResponse from "../interfaces/response.interface";
 import colors from "colors";
 
