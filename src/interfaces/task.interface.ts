e6ef220b6ce4b0d33491a5e63a13c487b973841b
majdshatti <majdshatti8@gmail.com>
2022-07-17 18:04:56 +0300

    Task interfaces added

--- src/interfaces/task.interface.ts
+++ src/interfaces/task.interface.ts
@@ -8,15 +8,15 @@ enum Status {
 
 interface taskInterface extends Document {
   name: string;
-  slug: string;
+  slug?: string;
   description?: string;
   status: Status;
   image?: string;
   startDate: Date;
   dueDate: Date;
   completeDate?: Date;
-  createdAt:Date;
-  updatedAt:Date;
+  createdAt?:Date;
+  updatedAt?:Date;
 }
 
 export default taskInterface;
