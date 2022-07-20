7c4032c8643a8e7aaf92d835149e8b4b74543464
majdshatti <majdshatti8@gmail.com>
2022-07-20 10:18:37 +0300

    Delete all record was added to the seeder

---
+++ src/seeder.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+// File Stream
+import fs from "fs";
+// Logging colors
+import colors from "colors";
+// Environment variables
+import dotenv from "dotenv";
+// Models
+import { User, Task } from "./model";
+
+//*********************************************/
+// * -c Insert constant data                  */
+// * -i insert test data                      */
+// * -t Delete all data                       */
+// * -d delete test data                      */
+//*********************************************/
+
+// Load environment variables
+dotenv.config({ path: __dirname + "/config/config.env" });
+
+// Connect to mongodb
+mongoose.connect(process.env.MONGO_URI);
+
+// Delete Data
+const deleteData = async () => {
+  try {
+    await User.deleteMany();
+    await Task.deleteMany();
+
+    console.log(colors.red.inverse("All data deleted..."));
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Execute one of the functions
+switch (process.argv[2]) {
+  case "-t":
+    deleteData();
+    break;
+  default:
+    throw "Invaild argument";
+}
