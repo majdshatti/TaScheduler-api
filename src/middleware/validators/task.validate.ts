fef56fc38cab2f7ff10e194f7f118e9477f68636
majdshatti <majdshatti8@gmail.com>
2022-07-21 12:12:11 +0300

    Task validation improved

--- src/middleware/validators/task.validate.ts
+++ src/middleware/validators/task.validate.ts
@@ -4,10 +4,22 @@ const taskValidate = (validationCase: "edit" | "create") => {
   switch (validationCase) {
     case "create":
       return [
+        validate("project")
+          .isRequired()
+          .isObjectId()
+          .isExist("Project", "_id")
+          .exec(),
+        validate("user")
+          .isRequired()
+          .isObjectId()
+          .isExist("User", "_id")
+          .exec(),
+        validate("todos").exec(),
+        validate("todos.*").isString().exec(),
         validate("name").isUnique("Task").isRequired().isString().exec(),
         validate("description").isLength(20, 200).exec(),
-        validate("startDate").isDate().exec(),
         validate("dueDate").isDate().exec(),
+        validate("startDate").isDate().isStartDateSmaller().exec(),
       ];
     case "edit":
       return [];
