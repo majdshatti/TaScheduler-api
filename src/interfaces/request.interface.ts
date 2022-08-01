632f26d1412d643f4b45ba9bf941d5f00a7b4e24
majdshatti <majdshatti8@gmail.com>
2022-08-01 20:53:16 +0300

    Finalize interfaces

--- src/interfaces/request.interface.ts
+++ src/interfaces/request.interface.ts
@@ -1,6 +1,7 @@
 import { IUserDocument } from "./";
 import { Request } from "express";
 
+// An inteface for holding req.user
 export interface IAuthRequest extends Request {
   user: IUserDocument;
 }
