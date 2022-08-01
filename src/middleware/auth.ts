1074b1bb30b15217da68a2421c25f168121e0abe
majdshatti <majdshatti8@gmail.com>
2022-08-01 21:17:08 +0300

    Finalize auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,12 +1,14 @@
-import { Request, Response, NextFunction } from "express";
+import { Response, NextFunction } from "express";
 import { asyncHandler } from "./";
 import { ErrorResponse, getErrorMessage, verifyToken } from "../utils";
 import { IAuthRequest } from "../interfaces";
+import { getUserById } from "../services/user.service";
 
 export const authenticate = asyncHandler(
   async (req: IAuthRequest, res: Response, next: NextFunction) => {
     let token: string = "";
 
+    // Check for a bearer token is sent form the client
     if (
       req.headers.authorization &&
       req.headers.authorization.startsWith("Bearer")
@@ -17,8 +19,17 @@ export const authenticate = asyncHandler(
       return next(new ErrorResponse(getErrorMessage("auth", "user"), 401));
 
     try {
-      const user = await verifyToken(token);
+      // Get a user id from token
+      const userId = await verifyToken(token);
 
+      // Check if user id is vailed
+      if (!userId)
+        return next(new ErrorResponse(getErrorMessage("auth", "user"), 401));
+
+      // Get user by id
+      const user = await getUserById(userId);
+
+      // Check if user exist
       if (!user)
         return next(new ErrorResponse(getErrorMessage("auth", "user"), 401));
 
